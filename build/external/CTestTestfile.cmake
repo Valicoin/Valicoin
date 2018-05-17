15b1ea6961763978225b8e4db39a6488ed69eb10
Valicoin <38242914+Hackerscape@users.noreply.github.com>
2018-05-16 22:08:38 -0700

    Remove Build Dir

--- build/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/lyxcoin/external
-# Build directory: /root/lyxcoin/build/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(miniupnpc)
-subdirs(gtest)
--- build/external/miniupnpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/lyxcoin/external/miniupnpc
-# Build directory: /root/lyxcoin/build/external/miniupnpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
